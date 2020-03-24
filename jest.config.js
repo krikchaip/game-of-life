b3804cc49bada1d040bc04a918a77632cfe7cf1c
krikchaip <krikchai.p@gmail.com>
2020-03-24 19:27:11 +0700

    setup: [jest] jsdom extend expect

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ module.exports = {
     '\\.css': 'identity-obj-proxy',
     '\\.(jpe?g|png)': '<rootDir>/test/assets-mock',
   },
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
   globals: {
     'ts-jest': {
       babelConfig: true,
