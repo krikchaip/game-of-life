bd0eb77769b288e4e1b4aceb198a5f5c94fc0740
krikchaip <krikchai.p@gmail.com>
2020-03-27 02:26:41 +0700

    add: [processor] state utils, simple nextState
    - readState
    - parseState
    - nextState

--- src/__tests__/processor.spec.ts
+++ src/__tests__/processor.spec.ts
@@ -1,31 +1,155 @@
-import { shouldAlive } from '../processor'
+import * as processor from '../processor'
 
-describe('shouldAlive', () => {
+describe('shouldAlive (classic)', () => {
   it('live cell,  <2 neighbors -> die', () => {
-    expect(shouldAlive(true, 0)).toBe(false)
-    expect(shouldAlive(true, 1)).toBe(false)
+    expect(processor.shouldAlive(true, 0)).toBe(false)
+    expect(processor.shouldAlive(true, 1)).toBe(false)
   })
 
   it('live cell, 2-3 neighbors -> live', () => {
-    expect(shouldAlive(true, 2)).toBe(true)
-    expect(shouldAlive(true, 3)).toBe(true)
+    expect(processor.shouldAlive(true, 2)).toBe(true)
+    expect(processor.shouldAlive(true, 3)).toBe(true)
   })
 
   it('live cell,  >3 neighbors -> die', () => {
-    expect(shouldAlive(true, 4)).toBe(false)
-    expect(shouldAlive(true, 5)).toBe(false)
+    expect(processor.shouldAlive(true, 4)).toBe(false)
+    expect(processor.shouldAlive(true, 5)).toBe(false)
   })
 
   it('dead cell,   3 neighbors -> live', () => {
-    expect(shouldAlive(false, 3)).toBe(true)
-    expect(shouldAlive(false, 2)).toBe(false)
-    expect(shouldAlive(false, 4)).toBe(false)
+    expect(processor.shouldAlive(false, 3)).toBe(true)
+    expect(processor.shouldAlive(false, 2)).toBe(false)
+    expect(processor.shouldAlive(false, 4)).toBe(false)
   })
 
   describe('validation', () => {
     it('throws negative neighbors', () => {
-      expect(() => shouldAlive(false, -1)).toThrow(expect.any(Error))
-      expect(() => shouldAlive(true, -1)).toThrow(expect.any(Error))
+      expect(() => processor.shouldAlive(false, -1)).toThrow(expect.any(Error))
+      expect(() => processor.shouldAlive(true, -1)).toThrow(expect.any(Error))
+    })
+  })
+})
+
+describe('readState', () => {
+  it('convert nested array to readable string', () => {
+    let narray = [
+      [false, false, false, false],
+      [false, true, true, false],
+      [false, true, true, false],
+      [false, false, false, false],
+    ]
+
+    expect(processor.readState(narray)).toMatchInlineSnapshot(`
+      "
+      x x x x
+      x o o x
+      x o o x
+      x x x x
+      "
+    `)
+
+    narray = [
+      [false, true, true, false],
+      [true, false, false, true],
+      [false, true, true, false],
+    ]
+
+    expect(processor.readState(narray)).toMatchInlineSnapshot(`
+      "
+      x o o x
+      o x x o
+      x o o x
+      "
+    `)
+  })
+})
+
+describe('parseState', () => {
+  it('convert state string to nested array', () => {
+    let sstring = `
+      x x x x
+      x o o x
+      x o o x
+      x x x x
+    `
+
+    expect(processor.parseState(sstring)).toEqual([
+      [false, false, false, false],
+      [false, true, true, false],
+      [false, true, true, false],
+      [false, false, false, false],
+    ])
+
+    sstring = `
+      x o o x
+      o x x o
+      x o o x
+    `
+
+    expect(processor.parseState(sstring)).toEqual([
+      [false, true, true, false],
+      [true, false, false, true],
+      [false, true, true, false],
+    ])
+  })
+
+  describe('validation', () => {
+    it('wrong character', () => {
+      expect(() =>
+        processor.parseState(`
+        - o o -
+        o - - o
+        - o o -
+      `)
+      ).toThrow(expect.any(Error))
+    })
+  })
+})
+
+describe('nextState (classic)', () => {
+  const spy = jest.spyOn(processor, 'shouldAlive')
+
+  afterEach(() => {
+    spy.mockClear()
+  })
+
+  describe('still lifes', () => {
+    it('block', () => {
+      const currentState = processor.parseState(`
+        x x x x
+        x o o x
+        x o o x
+        x x x x
+      `)
+      const nextState = processor.nextState(currentState, processor.shouldAlive)
+
+      expect(processor.readState(nextState)).toMatchInlineSnapshot(`
+        "
+        x x x x
+        x o o x
+        x o o x
+        x x x x
+        "
+      `)
+      expect(processor.shouldAlive).toBeCalled()
+    })
+
+    it('bee-hive', () => {
+      const currentState = processor.parseState(`
+        x o o x
+        o x x o
+        x o o x
+      `)
+      const nextState = processor.nextState(currentState, processor.shouldAlive)
+
+      expect(processor.readState(nextState)).toMatchInlineSnapshot(`
+        "
+        x o o x
+        o x x o
+        x o o x
+        "
+      `)
+      expect(processor.shouldAlive).toBeCalled()
     })
   })
 })
--- src/processor.ts
+++ src/processor.ts
@@ -7,3 +7,40 @@ export function shouldAlive(isAlive: boolean, neighbors: number) {
 
   return false
 }
+
+export function readState(currentState: boolean[][]) {
+  return (
+    currentState.reduce(
+      (acc, curr) => acc + '\n' + curr.map((s) => (s ? 'o' : 'x')).join(' '),
+      ''
+    ) + '\n'
+  )
+}
+
+export function parseState(stateString: string) {
+  return stateString
+    .trim()
+    .split(/\s{2,}/)
+    .map((row) =>
+      row.split(' ').map((c) => {
+        switch (c) {
+          case 'x':
+            return false
+          case 'o':
+            return true
+          default:
+            throw new Error(
+              "character must be only either 'x' or 'o' with single space between."
+            )
+        }
+      })
+    )
+}
+
+export function nextState(
+  currentState: boolean[][],
+  cellEvaluator: (isAlive: boolean, neighbors: number) => boolean
+) {
+  cellEvaluator(false, 0)
+  return currentState
+}
