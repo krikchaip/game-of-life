741504a704e96e9166c4c46bc73236ef68e0338e
krikchaip <krikchai.p@gmail.com>
2023-01-21 23:01:14 +0700

    configure vitest with TS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()]
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    setupFiles: [
+      '@testing-library/jest-dom',
+      'jest-styled-components',
+      'jest-axe/extend-expect'
+    ]
+  }
 })
