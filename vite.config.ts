711f024f26c6dc938a0a27720d1ca24a9152250e
krikchaip <krikchai.p@gmail.com>
2023-01-22 18:07:06 +0700

    fix vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import react from '@vitejs/plugin-react'
 
+import { name } from './package.json'
+
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  base: `/${name}/`,
   test: {
     globals: true,
     snapshotFormat: { printBasicPrototype: true },
